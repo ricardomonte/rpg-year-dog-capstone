d841caba6d922a1b90c8f307f8323d03d3645a74
ricardo <ramn21@gmail.com>
2021-03-05 00:34:27 +0100

    Add booscen

---
+++ src/scenes/BootScene.js
@@ -0,0 +1,13 @@
+import phaser from 'phaser';
+
+class BootScene extends Phaser.Scene {
+  constructor(){
+    super('Boot');
+  }
+
+  create() {
+    this.scene.start('PreloadScene')
+  }
+}
+
+export default BootScene;
