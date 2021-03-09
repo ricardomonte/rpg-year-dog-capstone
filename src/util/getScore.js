1a0da1a12e9709ae5751e3d37b45a997fc87547b
ricardo <ramn21@gmail.com>
2021-03-09 07:50:59 +0100

    Add getscre, setscore to fetcj API

---
+++ src/util/getScore.js
@@ -0,0 +1,33 @@
+export const getScore = async () => {
+  const response = await fetch(
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/N3rRWMIOpSUR1PI9iA3c/scores',
+    {
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    },
+  );
+  const result = await response.json();
+  const elements = await result;
+  elements.result.sort((a, b) => (a.score < b.score ? 1 : -1));
+  return elements;
+};
+
+export const setScore = async (user, score) => {
+  const data = {
+    user,
+    score,
+  };
+  await fetch(
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/N3rRWMIOpSUR1PI9iA3c/scores',
+    {
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      body: JSON.stringify(data),
+    },
+  );
+};
