3d2baf28421d3f75c494f1fff4c75332596cea00
ricardo <ramn21@gmail.com>
2021-03-09 07:37:54 +0100

    Add player player animation player fight

--- src/entities/Player.js
+++ src/entities/Player.js
@@ -1,5 +1,6 @@
-import phaser from 'phaser';
+import Phaser from 'phaser';
 import initAnimations from './PlayerAnimations';
+import collidable from '../mixin/collidable';
 
 class Player extends Phaser.Physics.Arcade.Sprite {
   constructor(scene, x, y) {
@@ -7,50 +8,57 @@ class Player extends Phaser.Physics.Arcade.Sprite {
     scene.add.existing(this);
     scene.physics.add.existing(this);
 
-    this.init()
-    this.initEvents()
+    Object.assign(this, collidable);
+
+    this.hp = 100;
+    this.init();
+    this.initEvents();
   }
 
   init() {
-    this.speed = 50
+    this.speed = 50;
     this.cursors = this.scene.input.keyboard.addKeys(
-      {'up': Phaser.Input.Keyboard.KeyCodes.W,
-      'down': Phaser.Input.Keyboard.KeyCodes.S,
-      'left': Phaser.Input.Keyboard.KeyCodes.A,
-      'right': Phaser.Input.Keyboard.KeyCodes.D,
-    })
+      {
+        up: Phaser.Input.Keyboard.KeyCodes.W,
+        down: Phaser.Input.Keyboard.KeyCodes.S,
+        left: Phaser.Input.Keyboard.KeyCodes.A,
+        right: Phaser.Input.Keyboard.KeyCodes.D,
+      },
+    );
 
-    initAnimations(this.scene.anims)
+    this.scene.input.keyboard.on('keydown-SPACE', () => {
+      this.play('player-attack', true);
+    });
+
+    initAnimations(this.scene.anims);
   }
 
   initEvents() {
-    this.scene.events.on(Phaser.Scenes.Events.UPDATE, this.update, this)
+    this.scene.events.on(Phaser.Scenes.Events.UPDATE, this.update, this);
   }
 
   update() {
-    const {up, down, left, right } = this.cursors
-    if(left.isDown){
-      this.setVelocityX(-this.speed)
-      this.setFlipX(true)
-      this.play('walk-side', true)
-    }else if(right.isDown ){
-      this.setVelocityX(this.speed)
-      this.setFlipX(false)
-      this.play('walk-side', true)
-    }else if(up.isDown){
-      this.setVelocityY(-this.speed)
-      this.play('up-walk', true)
-    }else if(down.isDown){
-      this.setVelocityY(this.speed)
-      this.play('down-walk', true)
-    }else {
-      this.setVelocity(0)
+    const {
+      up, down, left, right,
+    } = this.cursors;
+    if (left.isDown) {
+      this.setVelocityX(-this.speed);
+      this.setFlipX(true);
+      this.play('walk-side', true);
+    } else if (right.isDown) {
+      this.setVelocityX(this.speed);
+      this.setFlipX(false);
+      this.play('walk-side', true);
+    } else if (up.isDown) {
+      this.setVelocityY(-this.speed);
+      this.play('up-walk', true);
+    } else if (down.isDown) {
+      this.setVelocityY(this.speed);
+      this.play('down-walk', true);
+    } else {
+      this.setVelocity(0);
     }
-    this.body.velocity.normalize().scale(this.speed)
-  }
-
-  addCollider(layer, callback) {
-    this.scene.physics.add.collider(this, layer, callback, null,this)
+    this.body.velocity.normalize().scale(this.speed);
   }
 }
 
