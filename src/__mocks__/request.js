bc6afdf50ebfcd713518223c92d6af78596fc108
ricardo <ramn21@gmail.com>
2021-03-11 21:16:37 +0100

    Add API test

---
+++ src/__mocks__/request.js
@@ -0,0 +1,39 @@
+
+export const getScore = async () => {
+  const response = await fetch(
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/PO3xRC8bxrtbhRxaL7yU/scores',
+    {
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    },
+  );
+  const result = await response.json();
+  const elements = await result;
+  elements.result.sort((a, b) => (b.score - a.score));
+  return elements;
+};
+
+export const setScore = async (user, score) => {
+  const data = {
+    user,
+    score,
+  };
+  const response = await fetch(
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/PO3xRC8bxrtbhRxaL7yU/scores',
+    {
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      body: JSON.stringify(data),
+    },
+  );
+  const result = await response.json()
+  return result
+};
+
+
+export { getScores, postScore };
